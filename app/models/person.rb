e29a265168831879b8d1bf4585a03e2fcdda3e46
Felipe Matarazzo <felipemps@protonmail.com>
2021-05-05 10:26:02 -0300

    association belongs_to -> has_many correction in person and species

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,7 +6,7 @@ class Person < ApplicationRecord
   has_many :species, through: :species_people
 
   has_many :planet_people
-  belongs_to :planet, through: :planet_people
+  has_many :planets, through: :planet_people
 
   validates :name, :birth_year, :eye_color, :gender, :hair_color, :height, :mass, :skin_color, :homeworld, :url, presence: true
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -3,7 +3,7 @@ class Species < ApplicationRecord
   has_many :people, through: :species_people
 
   has_many :species_planets
-  belongs_to :planet, through: :species_planets
-  
+  has_many :planets, through: :species_planets
+
   validates :name, :classification, :designation, :average_height, :average_lifespan, :eye_colors, :hair_colors, :skin_colors, :language, :homeworld, :people, :url, presence: true
 end
