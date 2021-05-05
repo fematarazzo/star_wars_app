67ee72e09ed947011b91618e1dd1724f6ef14e3e
Felipe Matarazzo <felipemps@protonmail.com>
2021-05-05 09:53:49 -0300

    person model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_05_125230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "birth_year"
+    t.string "eye_color"
+    t.string "gender"
+    t.string "hair_color"
+    t.string "height"
+    t.string "mass"
+    t.string "skin_color"
+    t.string "homeworld"
+    t.text "species", default: [], array: true
+    t.text "starships", default: [], array: true
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
