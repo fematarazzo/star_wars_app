e34dac52a29c83609d9c99a0e87b9058fa862067
Felipe Matarazzo <felipemps@protonmail.com>
2021-05-05 11:15:45 -0300

    tdd tests for people and species fixed

--- test/models/species_test.rb
+++ test/models/species_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
+require 'json'
+require 'open-uri'
 
 class SpeciesTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @species = species(:valid)
+  end
+
+  test 'valid species' do
+    assert @species.valid?
+  end
+
+  test 'invalid without name' do
+    @species.name = nil
+    refute @species.valid?, 'saved species without a name'
+    assert_not_nil @species.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid without homeworld' do
+    @species.homeworld = nil
+    refute @species.valid?
+    assert_not_nil @species.errors[:homeworld]
+  end
+
+  test 'for name fetch using api' do
+    url = 'https://swapi.dev/api/species/'
+    species_serialized = URI.open(url).read
+    species = JSON.parse(species_serialized)
+
+    assert @species.name, species['results'].first['name']
+  end
 end
