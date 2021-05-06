b4e5521ced9afa25f325520f790fa259616c61e6
Felipe Matarazzo <felipemps@protonmail.com>
2021-05-06 11:40:40 -0300

    controller actions added and root path to people#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :planets, only: %i[index show]
   resources :species, only: %i[index show]
   resources :starships, only: %i[index show]
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'people#index'
 end
