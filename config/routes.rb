09702749b008ec214a1e2b73a2656358bcc86b16
Felipe Matarazzo <felipemps@protonmail.com>
2021-05-06 11:34:01 -0300

    routes corrected with resources format

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'starships/index'
-  get 'starships/show'
-  get 'species/index'
-  get 'species/show'
-  get 'planets/index'
-  get 'planets/show'
-  get 'people/index'
-  get 'people/show'
+  resources :people, only: %i[index show]
+  resources :planets, only: %i[index show]
+  resources :species, only: %i[index show]
+  resources :starships, only: %i[index show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
